28e91f2e5cecdaea90a80a250954c74156a62aaa
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-02-11 15:17:15 -0800

    fix: use clojure file name conventions

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,7 +2,7 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 
 export default {
-  input: "public/js/libra-chart.mjs",
+  input: "public/js/libra_chart.mjs",
   output: {
     file: "public/js/libra-chart.bundle.mjs",
     format: "esm",
