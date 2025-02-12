d3ba73ee1f90235597a327238e67710ebe22c431
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-02-12 12:00:25 -0800

    fix: clean up when no data

--- resources/cljs/libra_chart.cljs
+++ resources/cljs/libra_chart.cljs
@@ -12,27 +12,7 @@
 (def store
   (mobx/makeAutoObservable
    (atom
-    {:data
-     [{:date "2024-01-01" :weight 256}
-      {:date "2024-01-04" :weight 254}
-      {:date "2024-01-07" :weight 253}
-      {:date "2024-01-10" :weight 251}
-      {:date "2024-01-13" :weight 249}
-      {:date "2024-01-16" :weight 248}
-      {:date "2024-01-19" :weight 245}
-      {:date "2024-01-22" :weight 243}
-      {:date "2024-01-25" :weight 240}
-      {:date "2024-01-28" :weight 238}
-      {:date "2024-01-31" :weight 235}
-      {:date "2024-02-03" :weight 233}
-      {:date "2024-02-06" :weight 231}
-      {:date "2024-02-09" :weight 229}
-      {:date "2024-02-12" :weight 227}
-      {:date "2024-02-15" :weight 225}
-      {:date "2024-02-18" :weight 223}
-      {:date "2024-02-21" :weight 222}
-      {:date "2024-02-24" :weight 221}
-      {:date "2024-02-27" :weight 220}]})))
+    {:data []})))
 
 (def myChart (chart/Chart.
               (js/document.getElementById "libra")
@@ -60,24 +40,30 @@
                               :data
                               last
                               :date
-                              (date-fns/parse "yyyy-MM-dd" (js/Date.))
+                              (if
+                                (date-fns/parse "yyyy-MM-dd" (js/Date.))
+                                (js/Date.))
                               (date-fns/subDays 4))
                      :max (-> @store
                               :data
                               last
                               :date
-                              (date-fns/parse "yyyy-MM-dd" (js/Date.))
+                              (if
+                                (date-fns/parse "yyyy-MM-dd" (js/Date.))
+                                (js/Date.))
                               (date-fns/addDays 2))}
                  :y {:min (-> @store
                               :data
                               last
                               :weight
+                              (or 100)
                               (- 25))
 
                      :max (-> @store
                               :data
                               last
-                              :weight
+                              :weight 
+                              (or 100)
                               (+ 25))
                      :ticks {:stepSize 25}
                      :startAtZero true}}
