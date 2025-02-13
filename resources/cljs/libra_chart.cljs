1af02d3e5a18d9ac6af1b910b64ecd76fd5093e6
Berkeley True <berkeleytrue@users.noreply.github.com>
2025-02-12 18:58:30 -0800

    feat(chart): try adding a trend line

--- resources/cljs/libra_chart.cljs
+++ resources/cljs/libra_chart.cljs
@@ -6,14 +6,47 @@
    ["chartjs-plugin-annotation$default" :as annotations]
    ["date-fns" :as date-fns]
    ["mobx" :as mobx]
-   ["axios$default" :as axios]))
+   ["axios$default" :as axios]
+   ["regression$default" :as regression]))
 
 (apply (.-register chart/Chart) (into [] (concat chart/registerables [zoom annotations])))
 
 (def store
   (mobx/makeAutoObservable
    (atom
-    {:data []})))
+    {:data []
+     :trend-data []})))
+
+(defn calculate-trend-line [data]
+  (when (seq data)
+    (let [points (->> data
+                      (map (fn [point]
+                             [(-> point
+                                  :date
+                                  (date-fns/parse "yyyy-MM-dd" (js/Date.))
+                                  (.getTime))
+                              (:weight point)]))
+                      (reverse)
+                      (take 10)
+                      (reverse)
+                      (into []))
+          result (regression/linear points)
+          predict (fn [date]
+                    (let [x (.getTime (if (string? date)
+                                        (date-fns/parse date "yyyy-MM-dd" (js/Date.))
+                                        date))]
+                      (.predict result x)))
+          today (js/Date.)
+          last-date (-> data last :date)
+          last-date-obj (date-fns/parse last-date "yyyy-MM-dd" (js/Date.))
+          needs-prediction? (date-fns/isAfter today last-date-obj)]
+      (js/console.log "needs-prediction?" needs-prediction?)
+      (->> (if needs-prediction?
+             (conj data {:date (date-fns/format today "yyyy-MM-dd")
+                         :weight (second (predict today))
+                         :predicted true})
+             data)
+           (map (fn [point] (assoc point :trend (second (predict (:date point))))))))))
 
 (defn ->labels [data]
   (->> data
@@ -67,7 +100,16 @@
                                        (->data))
                             :fill false
                             :borderColor "rgb(75, 192, 192)"
-                            :tension 0.4
+                            :tension 0.6
+                            :pointStyle "rectRot"}
+                           {:label "Trend"
+                            :data (->> @store
+                                       :trend-data
+                                       (->data))
+                            :fill false
+                            :cubicInterpolationMode "monotone"
+                            :borderColor "rgb(255, 99, 132)"
+                            :tension 0.6
                             :pointStyle "rectRot"}]}
                :options
                {:scales
@@ -90,24 +132,30 @@
 (-> (axios/get "/api/data"
                {:responseType "json"})
     (.then (fn [response]
-             (swap! store assoc :data (-> response :data)))))
+             (let [data (-> response :data)
+                   trend-data (calculate-trend-line data)]
+               (swap! store assoc
+                      :data data
+                      :trend-data trend-data)))))
 
 (mobx/autorun
  (fn []
-   (let [data (->> @store
-                   :data
+   (let [trend-data (:trend-data @store)
+         data (->> trend-data
                    (map :weight)
                    (into []))
-         labels (->> @store
-                     :data
+         trend-line (->> trend-data
+                         (map :trend)
+                         (into []))
+         labels (->> trend-data
                      (map :date)
                      (into []))
-         x-range (->x-range (:data @store))
          y-range (->y-range (:data @store))]
      (set! (.. (first (.. myChart -data -datasets)) -data) data)
+     (set! (.. (get (.. myChart -data -datasets) 1) -data) trend-line)
      (set! (.. myChart -data -labels) labels)
-     (set! (.. myChart -options -scales -x -min) (:min x-range))
-     (set! (.. myChart -options -scales -x -max) (:max x-range))
+     ; (set! (.. myChart -options -scales -x -min) (:min x-range))
+     ; (set! (.. myChart -options -scales -x -max) (:max x-range))
      (set! (.. myChart -options -scales -y -min) (:min y-range))
      (set! (.. myChart -options -scales -y -max) (:max y-range))
      (myChart.update))))
